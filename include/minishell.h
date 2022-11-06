2a4f48ddeb39ec5371fa630575a8ed1d8205178d
Justin Morneau <jmorneau@c2r3p7.42quebec.com>
2022-11-06 16:50:40 -0500

    0.01

---
+++ include/minishell.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmorneau <jmorneau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/06 16:48:48 by jmorneau          #+#    #+#             */
+/*   Updated: 2022/11/06 16:48:50 by jmorneau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "color.h"
+# include "libft.h"
+
+# include "d_minishell.h"
+# include "p_minishell.h"
+# include "s_minishell.h"
+
+
+#endif
