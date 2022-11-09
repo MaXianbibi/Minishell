646b5c1c8eaff9787488bf95001e21cd3240e037
Dorian Wawzyniak <dwawzyni@c1r3p9.42quebec.com>
2022-11-09 02:07:06 -0500

    noob

--- src/Echo.c
+++ src/Echo.c
@@ -6,7 +6,7 @@
 /*   By: dwawzyni <dwawzyni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/06 16:56:39 by dwawzyni          #+#    #+#             */
-/*   Updated: 2022/11/09 01:36:58 by dwawzyni         ###   ########.fr       */
+/*   Updated: 2022/11/09 01:59:41 by dwawzyni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,14 @@
 
  void echo(char **splitcmd)
  {
+    // char *str2 = NULL;
     char *str;
-    char *str2;
+    (void)splitcmd;
     
     str = ft_strjoin(str,"\n");
-    str2 = splitcmd[1];
-    if(ft_strncmp(splitcmd[1],"-n",2) == 0)
-        ft_printf("%s",str);
-    else
-        ft_printf("%s",str2);
+    // str2 = splitcmd[1];
+    // if(ft_strncmp(splitcmd[1],"-n",2) == 0)
+        // ft_printf("%s",str2);
+    // else
+        printf("%s",str);
  }
